0a12520d3d11a8963547185fb7b00c4ba1ca2b71
cmchap <corymchapman@gmail.com>
2014-11-15 22:03:18 -0500

    First commit

---
+++ js/scripts-min.js
@@ -0,0 +1,23 @@
+/*jshint devel:true */
+$(document).ready( function() {
+    var book = [];
+    $.get("Alice-sentences.txt", function(data){
+        data = data.replace(/^/g, "/").replace(/\n/g, "\n/");
+        // console.log("Insert first /: "+data);
+        data = data.replace(/ /g, "/");
+        // console.log("replace spaces with /: "+data);
+        data = data.replace(/`/g,"'");
+        // console.log("replace backticks: "+data);
+        data = data.split('\n');
+        // console.log("Split: "+data);
+        book = data;
+    });
+    var counter = 0;
+
+    $("#next").click(function(){
+        window.history.pushState(null, "", book[counter]);
+        //console.log(book[counter]);
+        counter = (counter + 1) % book.length; // increment the counter & reset to 0 when it reaches the length of the array
+    });
+});
+
